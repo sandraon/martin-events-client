11547696967e483ecc9864ce90144990c9a984eb
Sandra <sandraonne@gmail.com>
2018-09-27 12:39:43 +0300

    Create controller 'home' and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :customer_events
   resources :event_categories
   resources :categories
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_092419) do
+ActiveRecord::Schema.define(version: 2018_09_27_093155) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customer_events", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "event_id"
+    t.integer "payment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_categories", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
