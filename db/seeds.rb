e7b8aaa50a48fe5133cda874a5a10a1146a000cf
Sandra <sandraonne@gmail.com>
2018-09-28 15:28:54 +0300

    Add default content as seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #Adding multiple categories
-Category.create([{name: 'Muusika'}, {name: 'Teater' }])
+Category.create([{name: 'Sport'}, {name: 'Festival' }, {name: 'EV100'}, {name: 'Film'}, {name: 'Kogupere'}, {name: 'Kinkekaardid'}, {name: 'Varia'}, {name: 'Näitus'}])
 
 #Adding one event
-Event.create({
+Event.create([{
    name: "Lorem ipsum",
    short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
    long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
    price: 3000,
-   date: Date.today + 3.weeks,
+   date: Date.today + 2.months + 3.days,
    location: "Tartu",
-   category: Category.where(name: "Muusika").first
-})
+   category: Category.where(name: "Sport").first
+}, {
+    name: "Lorem ipsum",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 3000,
+   date: Date.today - 1.year,
+   location: "Tallinn",
+   category: Category.where(name: "Film").first
+}, {
+    name: "Lorem ipsum",
+   short_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   long_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   price: 3000,
+   date: Date.new(2019, 8, 03),
+   location: "Tartu",
+   category: Category.where(name: "Näitus").first
+}])
+
+User.create({email:'sandra.onne@khk.ee', password: 'qwerty'}).confirm
