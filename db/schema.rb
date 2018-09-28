6d2944b46b1b57edea8b61157a0223702d5e7c1b
Sandra <sandraonne@gmail.com>
2018-09-28 10:23:16 +0300

    Add 'featured' to form and show events on homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_093155) do
+ActiveRecord::Schema.define(version: 2018_09_28_055742) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_093155) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "featured", default: false
   end
 
 end
