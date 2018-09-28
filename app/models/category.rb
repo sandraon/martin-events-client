f3352bdc5714aa2769b003d20df6efc4ef91190e
Sandra <sandraonne@gmail.com>
2018-09-28 14:24:25 +0300

    Fix relations and add events to category view

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,3 @@
 class Category < ApplicationRecord
-    has_many :event_category
-    has_many :events, through: :event_category
+    has_many :events
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,3 @@
 class Event < ApplicationRecord
-    has_one :event_category
-    has_one :category, through: :event_category
+    belongs_to :category
 end
