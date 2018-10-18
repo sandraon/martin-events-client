af9a5485b656e03bf304e8058f20aab122b1861c
Sandra <sandraonne@gmail.com>
2018-10-18 15:26:06 +0300

    Fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,10 +2,10 @@ class ApiController < ApplicationController
   skip_before_action :authenticate_user! 
   def about
     render json: {
-      time: Time.now.strftime("%H:%M:%S"),
+      time: Time.now.strftime("%H:%M"),
       date: Date.current,
       name: 'Sandra Õnne',
-      fruits: ['õun', 'pirn', 'apelsin']
+      fruits: ['õun', 'pirn', 'apelsin', 'banaan', 'greip']
     }
   end
 end
