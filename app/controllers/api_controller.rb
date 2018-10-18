283ede5f44d031043b7fa6788defea7e6256963b
Sandra <sandraonne@gmail.com>
2018-10-18 16:38:33 +0300

    Haiku api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,7 @@
 class ApiController < ApplicationController
   skip_before_action :authenticate_user! 
+  skip_before_action :verify_authenticity_token
+
   def about
     render json: {
       time: Time.now.strftime("%H:%M"),
@@ -7,5 +9,19 @@ class ApiController < ApplicationController
       name: 'Sandra Õnne',
       fruits: ['õun', 'pirn', 'apelsin', 'banaan', 'greip']
     }
+  end
+
+  def haiku
+    render json: {
+      haiku: reverse(haiku_params[:haiku]),
+    }
+  end
+
+  def reverse(haiku)
+    haiku.split(' ').reverse.join(' ')
+  end
+
+  def haiku_params
+    params.permit(:name, :haiku)
   end
 end
